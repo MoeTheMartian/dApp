b4a1ac20498478dc2cb5c7a27f391800cd64bf55
Gautier <Grant.Gautier@canfor.com>
2023-12-16 08:46:47 -0600

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Leaderboard from './components/Leaderboard';
+import Governance from './components/Governance';
+import Lottery from './components/Lottery';
+import Staking from './components/Staking';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* You can place a navbar or header content here */}
       </header>
+      <main>
+        <Leaderboard />
+        <Governance />
+        <Lottery />
+        <Staking />
+      </main>
+      <footer>
+        {/* And a footer here */}
+      </footer>
     </div>
   );
 }
